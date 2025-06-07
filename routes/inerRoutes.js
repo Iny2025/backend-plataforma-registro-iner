a84370f684fcb5ebc46d84e4b04df987dcdb7eea
Iny2025 <plataformainymori@gmail.com>
2025-06-06 21:25:55 -0400

    desporteger

--- routes/inerRoutes.js
+++ routes/inerRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post('/register', inerController.registerIner); // POST /api/iners/register (pública)
 router.post('/login', inerController.loginIner); // POST /api/iners/login (pública)
 router.get('/', verifyToken, inerController.getAllIner); // GET /api/iners (protegida)
-router.get('/:id', verifyToken, inerController.getInerById); // GET /api/iners/:id (protegida)
+router.get('/:id', inerController.getInerById); // GET /api/iners/:id (protegida)
 router.put('/:id', verifyToken, inerController.updateIner); // PUT /api/iners/:id (protegida)
 router.delete('/:id', verifyToken, inerController.deleteIner); // DELETE /api/iners/:id (protegida)
 
