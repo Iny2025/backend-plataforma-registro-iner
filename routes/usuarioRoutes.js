6d7cc8a8cf094e46481410065a2140521df72d84
Iny2025 <plataformainymori@gmail.com>
2025-06-25 16:01:14 -0400

    desproteger inerbyid

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post('/register', usuarioController.registerUsuario); // POST /api/usuarios/register (pública)
 router.post('/login', usuarioController.loginUsuario); // POST /api/usuarios/login (pública)
 router.get('/', verifyToken, usuarioController.getAllUsuarios); // GET /api/usuarios (protegida)
-router.get('/:id', verifyToken, usuarioController.getUsuarioById); // GET /api/usuarios/:id (protegida)
+router.get('/:id', usuarioController.getUsuarioById); // GET /api/usuarios/:id (protegida)
 router.put('/:id', verifyToken, usuarioController.updateUsuario); // PUT /api/usuarios/:id (protegida)
 router.delete('/:id', verifyToken, usuarioController.deleteUsuario); // DELETE /api/usuarios/:id (protegida)
 router.get('/rut/:rut', verifyToken, usuarioController.getUsuarioByRut); // GET /api/usuarios/rut/:rut (protegida)
